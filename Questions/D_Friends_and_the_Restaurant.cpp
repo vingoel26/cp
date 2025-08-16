d47f5c0b79c435889ca2522ec62da41bd0b54881
vingoel26 <vinayakgoel012@gmail.com>
2025-08-17 04:36:35 +0530

    solved: Questions/D_Friends_and_the_Restaurant.cpp

---
+++ Questions/D_Friends_and_the_Restaurant.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+#define int long long
+#define inp(n) \
+    int n;     \
+    cin >> n
+#define vin(a) \
+    for (int i = 0; i < n; ++i) \
+    {                           \
+        cin >> a[i];            \
+    }
+#define vout(a) \
+    for (int i = 0; i < n; ++i) \
+    {                           \
+        cout << a[i] << ' ';    \
+    }                           \
+    cout<<endl;
+#define pb push_back
+#define ff first
+#define ss second
+#define viv vector<vector<int>>
+#define nah cout << "NO\n";
+#define yah cout << "YES\n";
+#define be begin()
+#define en end()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define fast \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define vi vector<int>
+#define vpi vector<pair<int, int>>
+#define up upper_bound
+#define low lower_bound
+#define mod 1000000007
+using namespace std;
+
+/*
+========================================
+Author:         Vinayak Goel
+Institution:    IIITL
+May the WA avoid you
+========================================
+*/
+
+void solve()
+{
+    int n;
+    cin>>n;
+    vi a(n),b(n),c(n);
+    vin(a);
+    vin(b);
+    for(int i=0;i<n;i++){
+        c[i]=b[i]-a[i];
+    }
+    sort(all(c));
+    int ans=0;
+    int l=0,r=n-1;
+    // vout(c);
+    while(l<r){
+        if(c[l]+c[r]>=0){
+            ans++;
+            l++;
+            r--;
+        }
+        else{
+            l++;
+        }
+    }
+    cout<<ans<<endl;
+}
+
+int32_t main()
+{
+    fast int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
