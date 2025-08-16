e7cfdc9bbf8a290c7a9b415a715f657accb38895
vingoel26 <vinayakgoel012@gmail.com>
2025-08-17 01:52:07 +0530

    solved: Questions/C_Dora_and_Search.cpp

---
+++ Questions/C_Dora_and_Search.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+#define int long long
+#define inp(n) \
+    int n;     \
+    cin >> n
+#define vin(a) \
+    for (int i = 0; i < n; ++i) \
+    {                           \
+        cin >> a[i];            \
+    }
+#define vout(a) \
+    for (int i = 0; i < n; ++i) \
+    {                           \
+        cout << a[i] << ' ';    \
+    }
+#define pb push_back
+#define ff first
+#define ss second
+#define viv vector<vector<int>>
+#define nah cout << "NO\n";
+#define yah cout << "YES\n";
+#define be begin()
+#define en end()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define fast \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define vi vector<int>
+#define vpi vector<pair<int, int>>
+#define up upper_bound
+#define low lower_bound
+#define mod 1000000007
+using namespace std;
+
+/*
+========================================
+Author:         Vinayak Goel
+Institution:    IIITL
+May the WA avoid you
+========================================
+*/
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vi a(n);
+    vin(a);
+    int l=0,r=n-1,mx=n,mn=1;
+    while(l<=r){
+        if(a[r] == mx) {
+            mx--;
+            r--;
+        }
+        else if(a[r] == mn) {
+            mn++;
+            r--;
+        }
+        else if(a[l] == mn) {
+            mn++;
+            l++;
+        }
+        else if(a[l] == mx) {
+            mx--;
+            l++;
+        }
+        else {
+            break;
+        }
+    }
+    if(l>r){
+        cout<<-1<<endl;
+    }
+    else{
+        cout << l + 1 << ' ' << r + 1 << endl;
+    }
+}
+
+int32_t main()
+{
+    fast int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
