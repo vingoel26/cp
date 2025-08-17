d2cf84362826fa894d63bf3200e5ae7ad3e3a3e8
vingoel26 <vinayakgoel012@gmail.com>
2025-08-17 18:48:31 +0530

    solved: Questions/D_Divisible_Pairs.cpp Questions/D_Find_the_Different_Ones.cpp

---
+++ Questions/D_Find_the_Different_Ones.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+#define int long long
+#define inp(n) \
+    int n;     \
+    cin >> n
+#define vin(a)                  \
+    for (int i = 0; i < n; ++i) \
+    {                           \
+        cin >> a[i];            \
+    }
+#define vout(a)                 \
+    for (int i = 0; i < n; ++i) \
+    {                           \
+        cout << a[i] << ' ';    \
+    }
+#define pb push_back
+#define ff first
+#define ss second
+#define viv vector<vector<int>>
+#define nah cout << "NO\n";
+#define yah cout << "YES\n";
+#define be begin()
+#define en end()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define vi vector<int>
+#define vpi vector<pair<int, int>>
+#define up upper_bound
+#define low lower_bound
+#define mod 1000000007
+using namespace std;
+
+/*
+========================================
+Author:         Vinayak Goel
+Institution:    IIITL
+May the WA avoid you
+========================================
+*/
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    vector<int> ans(n, -1);
+
+    for (int i = 1; i <= n - 1; i++)
+    {
+        if (v[i] != v[i - 1])
+        {
+            ans[i] = i - 1;
+        }
+        else
+            ans[i] = ans[i - 1];
+    }
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        int l, r;
+        cin >> l >> r;
+        l--;
+        r--;
+        if (l <= ans[r])
+        {
+            cout << ans[r] + 1 << " ";
+            cout << r + 1 << " ";
+            cout << endl;
+        }
+        else
+        {
+            cout << -1 << " ";
+            cout << -1 << " ";
+            cout << endl;
+        }
+    }
+    cout << endl;
+}
+
+int32_t main()
+{
+    fast int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
