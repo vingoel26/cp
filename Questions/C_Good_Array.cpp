720f9a55dde7a95cf390e3c69425b5e3d4b0da0f
vingoel26 <vinayakgoel012@gmail.com>
2025-08-22 01:56:34 +0530

    solved: Questions/C_Good_Array.cpp

---
+++ Questions/C_Good_Array.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+#define int long long
+#define inp(n) \
+    int n;     \
+    cin >> n
+#define vin(a) \
+    for (int i = 0; i < n; ++i) \
+    {                           \
+        cin >> a[i];            \
+    }
+#define vout(a) \
+    for (int i = 0; i < n; ++i) \
+    {                           \
+        cout << a[i] << ' ';    \
+    }
+#define pb push_back
+#define ff first
+#define ss second
+#define viv vector<vector<int>>
+#define nah cout << "NO\n";
+#define yah cout << "YES\n";
+#define be begin()
+#define en end()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define fast \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define vi vector<int>
+#define vpi vector<pair<int, int>>
+#define up upper_bound
+#define low lower_bound
+#define mod 1000000007
+using namespace std;
+
+/*
+========================================
+Author:         Vinayak Goel
+Institution:    IIITL
+May the WA avoid you
+========================================
+*/
+
+void solve()
+{
+    int n;
+    cin>>n;
+    vector<pair<int,int>> a;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        a.pb({x,i+1});
+    }
+    sort(all(a));
+    vi px(n);
+    px[0]=a[0].first;
+    for(int i=1;i<n;i++){
+        px[i]=px[i-1]+a[i].first;
+    }
+    vi ans;
+    for(int i=0;i<n-1;i++){
+        if(2*(px[n-2]-a[i].first)==px[n-1]-a[i].first){
+            ans.pb(a[i].second);
+        }
+    }
+    if(px[n-2]==px[n-3]*2){
+        ans.pb(a[n-1].second);
+    }
+    cout<<ans.size()<<endl;
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int32_t main()
+{
+    fast int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
