03ae4b459e79f611ab1ff03e5282c4572a2b7231
vingoel26 <vinayakgoel012@gmail.com>
2025-08-21 12:37:56 +0530

    solved: Questions/quiksort.cpp

---
+++ Questions/quiksort.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int divide(vector<int> &a, int l, int h)
+{
+    int pivot = a[h];
+    int i = l - 1;
+    for (int j = l; j < h; j++)
+    {
+        if (a[j] < pivot)
+        {
+            i++;
+            swap(a[i], a[j]);
+        }
+    }
+    swap(a[i + 1], a[h]);
+    return i + 1;
+}
+
+void quicksort(vector<int> &a, int l, int h)
+{
+    if (l < h)
+    {
+        int pi = divide(a, l, h);
+        quicksort(a, l, pi - 1);
+        quicksort(a, pi + 1, h);
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    quicksort(a, 0, n - 1);
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
