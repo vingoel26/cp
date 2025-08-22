db6aff35d86b18dc3604861898a63772ca73d6e8
vingoel26 <vinayakgoel012@gmail.com>
2025-08-23 01:08:49 +0530

    solved: Questions/A_Array_Elimination.cpp

---
+++ Questions/A_Array_Elimination.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+#define int long long
+#define inp(n) \
+    int n;     \
+    cin >> n
+#define vin(a)                  \
+    for (int i = 0; i < n; ++i) \
+    {                           \
+        cin >> a[i];            \
+    }
+#define vout(a)                 \
+    for (int i = 0; i < n; ++i) \
+    {                           \
+        cout << a[i] << ' ';    \
+    }
+#define pb push_back
+#define ff first
+#define ss second
+#define viv vector<vector<int>>
+#define nah cout << "NO\n";
+#define yah cout << "YES\n";
+#define be begin()
+#define en end()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define vi vector<int>
+#define vpi vector<pair<int, int>>
+#define up upper_bound
+#define low lower_bound
+#define mod 1000000007
+using namespace std;
+
+/*
+========================================
+Author:         Vinayak Goel
+Institution:    IIITL
+May the WA avoid you
+========================================
+*/
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vi a(n), b(31);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        for (int j = 0; j < 31; j++)
+        {
+            if (a[i] & (1LL << j))
+            {
+                b[j]++;
+            }
+        }
+    }
+    vi ans;
+    ans.pb(1);
+    for (int i = 2; i <= n; i++)
+    {
+        bool q = true;
+        for (int j = 0; j < 31; j++)
+        {
+            if (b[j] % i != 0)
+            {
+                q = false;
+                continue;
+            }
+        }
+        if (q)
+        {
+            ans.pb(i);
+        }
+    }
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int32_t main()
+{
+    fast int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
