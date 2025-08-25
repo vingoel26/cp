af57125336669ddb95ad93d83d5239a84291bba1
vingoel26 <vinayakgoel012@gmail.com>
2025-08-25 14:35:36 +0530

    solved: Questions/Make the String k-Beautiful.cpp

--- Questions/Make the String k-Beautiful.cpp
+++ Questions/Make the String k-Beautiful.cpp
@@ -6,20 +6,11 @@ int main()
     cin >> n >> k;
     string s;
     cin >> s;
-    // int n1=(n+k-1)/k;
     vector<vector<char>> v(k);
     for (int i = 0; i < n; i++)
     {
         v[i % k].push_back(s[i]);
     }
-    // for (int i = 0; i < k; ++i)
-    // {
-    //     for (char c : v[i])
-    //     {
-    //         cout << c << " ";
-    //     }
-    //     cout << endl;
-    // }
     int ans = 0;
     for (auto it : v)
     {
