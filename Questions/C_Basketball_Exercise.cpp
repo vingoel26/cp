5b9ca0bdf1e0cf56b2aca148e67bfb6883fd08a5
vingoel26 <vinayakgoel012@gmail.com>
2025-08-26 19:06:07 +0530

    solved: Questions/C_Basketball_Exercise.cpp

---
+++ Questions/C_Basketball_Exercise.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#define int long long
+#define inp(n) \
+    int n;     \
+    cin >> n
+#define vin(a) \
+    for (int i = 0; i < n; ++i) \
+    {                           \
+        cin >> a[i];            \
+    }
+#define vout(a) \
+    for (int i = 0; i < n; ++i) \
+    {                           \
+        cout << a[i] << ' ';    \
+    }
+#define pb push_back
+#define ff first
+#define ss second
+#define viv vector<vector<int>>
+#define nah cout << "NO\n";
+#define yah cout << "YES\n";
+#define be begin()
+#define en end()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define fast \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define vi vector<int>
+#define vpi vector<pair<int, int>>
+#define up upper_bound
+#define low lower_bound
+#define mod 1000000007
+using namespace std;
+
+/*
+========================================
+Author:         Vinayak Goel
+Institution:    IIITL
+May the WA avoid you
+========================================
+*/
+
+void solve()
+{
+    int n;
+    cin>>n;
+    vi a(n),b(n);
+    vin(a);
+    vin(b);
+    vector<vector<int>>dp(n+1,vector<int>(3,0));
+    for(int i=1;i<=n;i++){
+        dp[i][0]=max(dp[i-1][1]+a[i-1],max(dp[i-1][2]+a[i-1],a[i-1]));
+        dp[i][1]=max(dp[i-1][0]+b[i-1],max(dp[i-1][2]+b[i-1],b[i-1]));
+        dp[i][2]=max(dp[i-1][1],dp[i-1][0]);
+    }
+    cout<<max(dp[n][0],dp[n][1])<<endl;
+}
+
+int32_t main()
+{
+    fast int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
