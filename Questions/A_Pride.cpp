8c17c27b6487d020e0e5adfa386b56d4aad94892
vingoel26 <vinayakgoel012@gmail.com>
2025-09-02 16:41:10 +0530

    solved: Questions/A_Pride.cpp

---
+++ Questions/A_Pride.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+#define int long long
+#define inp(n) \
+    int n;     \
+    cin >> n
+#define vin(a) \
+    for (int i = 0; i < n; ++i) \
+    {                           \
+        cin >> a[i];            \
+    }
+#define vout(a) \
+    for (int i = 0; i < n; ++i) \
+    {                           \
+        cout << a[i] << ' ';    \
+    }
+#define pb push_back
+#define ff first
+#define ss second
+#define viv vector<vector<int>>
+#define nah cout << "NO\n";
+#define yah cout << "YES\n";
+#define be begin()
+#define en end()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define fast \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define vi vector<int>
+#define vpi vector<pair<int, int>>
+#define up upper_bound
+#define low lower_bound
+#define mod 1000000007
+using namespace std;
+
+/*
+========================================
+Author:         Vinayak Goel
+Institution:    IIITL
+May the WA avoid you
+========================================
+*/
+
+void solve()
+{
+    int n;
+    cin>>n;
+    vi a(n);
+    vin(a);
+    int g=a[0];
+    for(int i=1;i<n;i++){
+        g=__gcd(g,a[i]);
+    }
+    if(g!=1){
+        cout<<-1<<endl;
+        return;
+    }
+    int ct=0;
+    for(int i=0;i<n;i++){
+        if(a[i]==1){
+            ct++;
+        }
+    }
+    if(ct!=0){
+        cout<<n-ct<<endl;
+        return;
+    }
+    int ans=1e9;
+    for(int i=0;i<n;i++){
+        int g=a[i];
+        for(int j=i;j<n;j++){
+            g=__gcd(g,a[j]);
+            // cout<<g<<" ";
+            if(g==1){
+                // cout<<"yaee"<<endl;
+                ans=min(ans,j-i);
+                break;
+            }
+        }
+        // cout<<endl;
+    }
+    cout<<n-1+ans<<endl;
+}
+
+int32_t main()
+{
+    fast int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
