bf341df385eb9b85ba6f80f5613d556c31a8d92d
vingoel26 <vinayakgoel012@gmail.com>
2025-09-03 13:45:34 +0530

    solved: Questions/F_Greetings.cpp

---
+++ Questions/F_Greetings.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define int long long
+#define inp(n) \
+    int n;     \
+    cin >> n
+#define vin(a) \
+    for (int i = 0; i < n; ++i) \
+    {                           \
+        cin >> a[i];            \
+    }
+#define vout(a) \
+    for (int i = 0; i < n; ++i) \
+    {                           \
+        cout << a[i] << ' ';    \
+    }
+#define pb push_back
+#define ff first
+#define ss second
+#define viv vector<vector<int>>
+#define nah cout << "NO\n";
+#define yah cout << "YES\n";
+#define be begin()
+#define en end()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define fast \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define vi vector<int>
+#define vpi vector<pair<int, int>>
+#define up upper_bound
+#define low lower_bound
+#define mod 1000000007
+using namespace std;
+
+typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;
+// order_of_key() , find_by_order()
+//int M = 3*(1e5);
+int pinf = INT_MAX;
+int ninf = INT_MIN;
+
+/*
+========================================
+Author:         Vinayak Goel
+Institution:    IIITL
+May the WA avoid you
+========================================
+*/
+
+void solve()
+{
+    int n;
+    cin>>n;
+    vpi v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i].first>>v[i].second;
+    }
+    sort(all(v));
+    ordered_set st;
+    for (int i = 0; i < n; i++) {
+        st.insert(v[i].second);
+    }
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        ans += st.order_of_key(v[i].second);
+        st.erase(v[i].second);
+    }
+    cout<<ans<<endl;
+}
+
+int32_t main()
+{
+    fast int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
